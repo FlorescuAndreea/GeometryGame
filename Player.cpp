628b3252eee006db6402e91bc2b8fbba3a137119
Andreea Florescu <andreeaflorescu_15@yahoo.com>
2013-10-28 01:12:46 +0200

    initial commit for files

---
+++ Player.cpp
@@ -0,0 +1,89 @@
+#include "Player.h"
+
+Player::Player(float x, float y, Color color) {
+
+	this->epsilon = 30;
+
+	this->weaponOn = false;
+	this->speed = 1;
+	this->x = x;
+	this->y = y;
+	this->radius = 25;
+	this->u = 0;
+	this->weaponWidth = 90;
+
+	//Add Player
+	Player::poligon = new Polygon2D(color, false);
+	cerc = new Circle2D(Point2D(x, y), this->radius, color, false);
+	poligon->addPoint(Point2D(x-20, y));
+	poligon->addPoint(Point2D(x, y + 20));
+	poligon->addPoint(Point2D(x + 20, y + 10));
+	poligon->addPoint(Point2D(x, y + 17));
+	poligon->addPoint(Point2D(x - 10, y));
+	poligon->addPoint(Point2D(x, y - 18));
+	poligon->addPoint(Point2D(x + 20, y - 10));
+	poligon->addPoint(Point2D(x, y - 20));
+	//End Player
+
+	//Add Weapon
+	weapon = new Polygon2D(color, true);
+	weapon->addPoint(Point2D(this->x + 30, this->y + 15));
+	weapon->addPoint(Point2D(this->x + 90, this->y));
+	weapon->addPoint(Point2D(this->x + 30, this->y - 15));
+	//End Weapon
+}
+
+Player::~Player() {
+	
+}
+
+void Player::addPlayer(Visual2D *playGround) {
+	DrawingWindow::addObject2D_to_Visual2D(cerc, playGround);
+	DrawingWindow::addObject2D_to_Visual2D(poligon, playGround);
+}
+
+void Player::rotatePlayer(float u) {
+
+	this->u += u;
+
+	Transform2D::loadIdentityMatrix();
+	Transform2D::translateMatrix(-this->x, -this->y);
+	Transform2D::rotateMatrix(u);
+	Transform2D::translateMatrix(this->x, this->y);
+	Transform2D::applyTransform_o(poligon);
+	Transform2D::applyTransform_o(weapon);
+}
+
+void Player::activateWeapon(Visual2D *playGround) {
+	weaponOn = true;
+	DrawingWindow::addObject2D_to_Visual2D(weapon, playGround);
+}
+
+void Player::deactivateWeapon(Visual2D *playGround) {
+	weaponOn = false;
+	DrawingWindow::removeObject2D_from_Visual2D(weapon, playGround);
+}
+
+void Player::translatePlayer() {
+	float newX, newY;
+	newX = this->x + radius*cos(u);
+	newY = this->y + radius*sin(u);
+
+	if(newX + weaponWidth < DrawingWindow::width && newX - weaponWidth > 0 && //x is in window
+		newY + weaponWidth + 100 - epsilon < DrawingWindow::height && newY - weaponWidth > 0 //y is in window
+	   ) {
+
+		this->x = newX;
+		this->y = newY;
+		Transform2D::loadIdentityMatrix();
+		Transform2D::translateMatrix(radius*cos(u), radius* sin(u));
+		Transform2D::applyTransform_o(poligon);
+		Transform2D::applyTransform_o(cerc);
+		Transform2D::applyTransform_o(weapon);
+	}
+	cout << y << "\n";
+}
+
+bool Player::isWeaponOn() {
+	return this->weaponOn;
+}
---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include "Framework/DrawingWindow.h"
+#include "Framework/Visual2D.h"
+#include "Framework/Transform2D.h"
+#include "Framework/Line2D.h"
+#include "Framework/Rectangle2D.h"
+#include "Framework/Circle2D.h"
+#include "Framework/Polygon2D.h"
+#include <iostream>
+#include <windows.h>
+#include "Player.h"
+
+
+#define PI 3.14159265358979323846
+
+using namespace std;
+
+Visual2D *playGround;
+Visual2D *headsUpDisplay;
+Rectangle2D *rect;
+Circle2D *cerc;
+Polygon2D *poligon;
+Color *portocaliu = new Color(1, 0.498039, 0.313725);
+Player *player = new Player(100, 100, *portocaliu);	
+
+Text *text;
+
+//functia care permite adaugarea de obiecte
+void DrawingWindow::init()
+{
+	playGround = new Visual2D(0,0,DrawingWindow::width,DrawingWindow::height,0,100,DrawingWindow::width,DrawingWindow::height);  
+	addVisual2D(playGround);
+	player->addPlayer(playGround);
+
+	headsUpDisplay = new Visual2D(0,0,DrawingWindow::width,DrawingWindow::height,0,0,DrawingWindow::width,100);
+	addVisual2D(headsUpDisplay);
+	addObject2D_to_Visual2D(new Line2D(Point2D(0, 99), Point2D(DrawingWindow::width,99), Color(1, 0.498039, 0.313725)),headsUpDisplay);
+}
+
+
+//functia care permite animatia
+void DrawingWindow::onIdle()
+{
+
+}
+
+//functia care se apeleaza la redimensionarea ferestrei
+void DrawingWindow::onReshape(int width,int height)
+{
+	
+	playGround->poarta(0,0,width,height); 
+}
+
+//functia care defineste ce se intampla cand se apasa pe tastatura
+void DrawingWindow::onKey(unsigned char key)
+{
+	switch(key)
+	{
+		case 27: 
+			exit(0);
+		case KEY_UP:
+			player->translatePlayer();
+			break;
+		case KEY_SPACE:
+			if (player->isWeaponOn()) {
+				player->deactivateWeapon(playGround);
+			} else {
+				player->activateWeapon(playGround);
+			}
+			break;
+		case KEY_RIGHT:
+			player->rotatePlayer(0.1);
+			break;
+		case KEY_LEFT:
+			player->rotatePlayer(-0.1);
+			break;
+	}
+}
+
+//functia care defineste ce se intampla cand se da click pe mouse
+void DrawingWindow::onMouse(int button,int state,int x, int y)
+{
+	
+}
+
+
+int main(int argc, char** argv)
+{
+	//creare fereastra
+	DrawingWindow dw(argc, argv, 1300, 700, 0, 0, "Laborator EGC");
+	//se apeleaza functia init() - in care s-au adaugat obiecte
+	dw.init();
+	//se intra in bucla principala de desenare - care face posibila desenarea, animatia si procesarea evenimentelor
+	dw.run();
+	return 0;
+
+}
